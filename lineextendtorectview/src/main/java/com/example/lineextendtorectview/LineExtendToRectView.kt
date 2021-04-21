6b96f11f65de0ed979a1acb972e5fecf3a343aba
Anwesh43 <anweshthecool0@gmail.com>
2021-04-22 02:09:28 +0530

    adding constants and libraries

--- lineextendtorectview/src/main/java/com/example/lineextendtorectview/LineExtendToRectView.kt
+++ lineextendtorectview/src/main/java/com/example/lineextendtorectview/LineExtendToRectView.kt
@@ -1,2 +1,27 @@
 package com.example.lineextendtorectview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#FF5722",
+    "#00C853",
+    "#01579B"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
