af7e92e6d905f2eb3fb9be4dabcf6f8ba6716968
Anwesh43 <anweshthecool0@gmail.com>
2021-04-22 02:14:12 +0530

    adding Canvas extension methods

--- lineextendtorectview/src/main/java/com/example/lineextendtorectview/LineExtendToRectView.kt
+++ lineextendtorectview/src/main/java/com/example/lineextendtorectview/LineExtendToRectView.kt
@@ -30,3 +30,26 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineExtendToRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sf3)
+    drawLine( -size * 0.5f * sf1, 0f, size * 0.5f * sf1, 0f, paint)
+    drawRect(RectF(-size / 2, 0f, size / 2, size * sf2), paint)
+    restore()
+}
+
+fun Canvas.drawLETRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineExtendToRect(scale, w, h, paint)
+}
